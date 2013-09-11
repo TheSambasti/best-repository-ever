ab64384d1b120b367ce78df7de54c89db67eb62a
Kai Si <kaisi777@yahoo.com>
2013-09-10 19:55:56 -0700

    i edited it

--- test.py
+++ test.py
@@ -5,14 +5,10 @@ i = 0
 while i < 10000:
     print(i)
     i+= i*i**(1/7)//7 + 2
-<<<<<<< HEAD
 
-=======
 def hi():
     print('hi, this function was made by me')
     i = 0
     while i < 100:
         print('This is the '+i+'th instance of this')
         i += 1
->>>>>>> f0d52ecb273b01df0ea47601119e19508c74a67e
-
